3d21c50a778cc05a2b4fb5eb1bc4c85760ac0fbf
Erik Demchak <demcsakerik@gmail.com>
2024-08-17 22:12:38 +0200

    add: apartments CRUD

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.send('Healthy endpoint');
 });
+
+
+app.get("/apartments", (req, res) => {
+  res.send('Implement apartments list with offset and limit');
+});
+
+app.get("/apartments/:id", (req, res) => {
+  res.send('Implement getting apartment by id');
+});
+
+app.post("/apartments", (req, res) => {
+  res.send('Implement apartment create endpoint');
+});
+
+app.put("/apartments/:id", (req, res) => {
+  res.send('Implement apartment update by id');
+});
+
+app.delete("/apartments/:id", (req, res) => {
+  res.send('Implement apartment deletion by id');
+});
+
+
+
